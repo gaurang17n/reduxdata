48061dc8b1981199ac1e2afb8747043c641a2c52
gaurang17n <96428371+gaurang17n@users.noreply.github.com>
2022-03-22 08:37:56 +0530

    xyz
    add

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,75 +1,71 @@
-import React,{useEffect, useState,Component} from 'react';
-import { Container,Row,Col,Card,CardGroup, Button} from 'react-bootstrap';
-import Header from './Header';
-import {ADD_CART,FetchProductRequest,InsertData} from '../Actions/Index'
-import { connect } from 'react-redux';
-
+import React, { useEffect, useState, Component } from "react";
+import { Container, Row, Col, Card, CardGroup, Button } from "react-bootstrap";
+import Header from "./Header";
+import { ADD_CART, FetchProductRequest, InsertData } from "../Actions/Index";
+import { connect } from "react-redux";
 
 class Product extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     /* this.state = {
        first
     } */
   }
-  componentDidMount(){
-    this.props.FetchProductRequest()
-    console.log("product data fetch"+JSON.stringify(this.props.products))
+  componentDidMount() {
+    this.props.FetchProductRequest();
+    console.log("product data fetch" + JSON.stringify(this.props.products));
   }
-
-
 
   render() {
-    const ProductData = this.props.products.Products
+    const ProductData = this.props.products.Products;
     return (
-      <div><Container>
-        
-      <Header/>
-      <Row>
-        
-        {ProductData.length > 0 ?
-        ProductData.map((Pdata,Index)=>(
-          <Col key={Index}>
-          <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>{Pdata.id}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                    <Card.Text>
-                    {Pdata.title}
-                    </Card.Text>
-                    <Card.Link href="#">{Pdata.author}</Card.Link><br/>
-                    <Button onClick={()=>this.props.InsertData(Pdata)}> Add to Cart</Button>
-                </Card.Body>  
-                </Card>
-          </Col>
-        )) : <h3> No Data </h3>
-            }
-      </Row>
- 
-</Container></div>
-    )
+      <div>
+        <Container>
+          <Header />
+          <Row>
+            {ProductData.length > 0 ? (
+              ProductData.map((Pdata, Index) => (
+                <Col key={Index}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>{Pdata.id}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        Card Subtitle
+                      </Card.Subtitle>
+                      <Card.Text>{Pdata.title}</Card.Text>
+                      <Card.Link href="#">{Pdata.author}</Card.Link>
+                      <br />
+                      <Button onClick={() => this.props.InsertData(Pdata)}>
+                        {" "}
+                        Add to Cart
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))
+            ) : (
+              <h3> No Data </h3>
+            )}
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
 
-
-
-  
-const mapStateToProps=(state)=>{
+const mapStateToProps = (state) => {
   /* console.log("state" + JSON.stringify(state)) */
   return {
-    products:state._ToDoProduct
-  }
-}
+    products: state._ToDoProduct,
+  };
+};
 
-const mapDispatchToProps=(dispatch)=>{
-  return{
-    FetchProductRequest:()=>dispatch(FetchProductRequest()),
-    InsertData:item=>dispatch(InsertData(item))
-    
-  }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    FetchProductRequest: () => dispatch(FetchProductRequest()),
+    InsertData: (item) => dispatch(InsertData(item)),
+  };
+};
 
-
-export default connect(mapStateToProps,mapDispatchToProps)(Product)
-
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
